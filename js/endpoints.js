31902c53305f9a1c9567ff5ea0c7055b93ebd04b
IsmaDevs <chavezpedrito04@gmail.com>
2024-04-26 18:50:22 -0300

    Endpoints exportados e importados en App.js para las cards

---
+++ js/endpoints.js
@@ -0,0 +1,3 @@
+export const CELULARES = 'http://localhost:3001/Celulares';
+export const TELEVISORES = 'http://localhost:3001/Televisores';
+export const LAVARROPAS = 'http://localhost:3001/Lavarropas';
